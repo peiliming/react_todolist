b54924e6550a3c7850452fec9dd7cb1fe29f28be
Mingming <peiliming910@gmail.com>
2023-04-05 10:21:08 +0900

    eventを一応作成しておく

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,24 @@ function App() {
     inputValue: string;
     id: number;
     checked: boolean;
-  }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+  };
 
 
   return (
     <div className="App">
       <div>
         <h2>Todoリスト</h2>
-        <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="inputText" />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e) => handleChange(e)} className="inputText" />
           <input type="submit" value="作成" className="submitButton" />
         </form>
       </div>
